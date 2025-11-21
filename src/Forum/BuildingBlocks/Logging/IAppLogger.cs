533e1994dcb9317b4175331dd8fb1e2c45fc7b4c
Fábio Maiorano <fsmaiorano@gmail.com>
2025-11-21 10:14:03 -0300

    Implement AppLogger class to enhance logging capabilities

--- src/Forum/BuildingBlocks/Logging/IAppLogger.cs
+++ src/Forum/BuildingBlocks/Logging/IAppLogger.cs
@@ -6,37 +6,3 @@ public interface IAppLogger<T>
     void LogWarning(LogType logType, string message, params object[] args);
     void LogError(LogType logType, Exception? exception, string message, params object[] args);
 }
-
-public class AppLogger<T> : IAppLogger<T>
-{
-    private readonly ILogger<T> _logger;
-
-    public AppLogger(ILogger<T> logger)
-    {
-        _logger = logger;
-    }
-
-    public void LogInformation(LogType logType, string message, params object[] args)
-    {
-        Log(LogLevel.Information, logType, null, message, args);
-    }
-
-    public void LogWarning(LogType logType, string message, params object[] args)
-    {
-        Log(LogLevel.Warning, logType, null, message, args);
-    }
-
-    public void LogError(LogType logType, Exception? exception, string message, params object[] args)
-    {
-        Log(LogLevel.Error, logType, exception, message, args);
-    }
-
-    private void Log(LogLevel logLevel, LogType logType, Exception? exception, string message, params object[] args)
-    {
-        using (_logger.BeginScope(new Dictionary<string, object> { ["LogType"] = logType.ToString() }))
-        {
-            _logger.Log(logLevel, exception, message, args);
-        }
-    }
-}
-
