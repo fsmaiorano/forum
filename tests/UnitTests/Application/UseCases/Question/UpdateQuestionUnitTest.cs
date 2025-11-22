70b6a1fd56df226cd9df7e9466cc0f8210967d54
Fábio Maiorano <fsmaiorano@gmail.com>
2025-11-22 20:46:39 -0300

    Add unit test for UpdateQuestionUseCase to verify exception handling for non-existent questions

--- tests/UnitTests/Application/UseCases/Question/UpdateQuestionUnitTest.cs
+++ tests/UnitTests/Application/UseCases/Question/UpdateQuestionUnitTest.cs
@@ -1,5 +1,5 @@
 using Forum.Application.UseCases.Question.UpdateQuestion;
-using Forum.Domain.Entities.ValuesObjects;
+using Forum.BuildingBlocks.Exceptions;
 using UnitTests.Factories;
 
 namespace UnitTests.Application.UseCases.Question;
@@ -21,15 +21,36 @@ public class UpdateQuestionUnitTest(DatabaseFixture fixture) : BaseTest(fixture)
             questionId: question.Id.ToString(),
             authorId: question.AuthorId.ToString(),
             title: question.Title + "_Updated",
-            content: question.Content,
+            content: question.Content + "_Updated",
             slug: question.Slug?.Value
         );
-        
+
         var result = await useCase.UpdateQuestionUseCaseHandler(command);
         var updatedQuestion = await repository.FindById(question.Id.ToString());
-        
+
         Assert.NotNull(updatedQuestion);
         Assert.Equal(command.Title, updatedQuestion.Title);
         Assert.Equal(command.Content, updatedQuestion.Content);
+    }
+
+    [Fact]
+    public async Task UpdateQuestionUseCaseHandler_ShouldThrowNotFoundException_WhenQuestionDoesNotExist()
+    {
+        var repository = new QuestionRepository(Context);
+        var attachmentRepository = new AttachmentRepository(Context);
+        var loggerMock = CreateLoggerMock<UpdateQuestionUseCase>();
+        var useCase = new UpdateQuestionUseCase(loggerMock.Object, repository, attachmentRepository);
+
+        var command = MakeQuestion.UpdateQuestionCommand(
+            questionId: Guid.NewGuid().ToString(),
+            authorId: Guid.NewGuid().ToString(),
+            title: "Sample Title",
+            content: "Sample Content"
+        );
+
+        await Assert.ThrowsAsync<NotFoundException>(async () =>
+        {
+            await useCase.UpdateQuestionUseCaseHandler(command);
+        });
     }
 }
