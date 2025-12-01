b7cd2b14174da5f35400f4fd393dac83855f9ab2
Fábio Maiorano <fsmaiorano@gmail.com>
2025-12-01 19:16:53 -0300

    Add StringExtensions class with Excerpt method for content truncation

---
+++ src/BuildingBlocks/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+namespace BuildingBlocks.Extensions;
+
+public static class StringExtensions
+{
+    public static string Excerpt(this string content, int maxLength = 200)
+    {
+        if (string.IsNullOrEmpty(content))
+            return string.Empty;
+
+        return content.Length <= maxLength ? content : string.Concat(content.AsSpan(0, maxLength), "...");
+    }
+}
